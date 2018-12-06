3f985fae0a59e90b315a12df9a5778482520cb92
JUARL016 <Levy.Ulises.X.Juarez.Dorantes.-ND@disney.com>
2018-12-06 11:53:00 -0600

    Adding one component test

--- src/test/java/com/ksquareinc/testing/sonartest/component/UserComponentTest.java
+++ src/test/java/com/ksquareinc/testing/sonartest/component/UserComponentTest.java
@@ -1,5 +1,22 @@
 package com.ksquareinc.testing.sonartest.component;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import io.restassured.RestAssured;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserComponentTest {
 
-}
+	
+		@Test
+		public void testWeShoulwGet200() {
+			RestAssured.given().baseUri("http://localhost:8080/api").when().get("/users/").then().assertThat().statusCode(200).and().body("size()", equalTo(2));
+		}
+		
+	}
+
+
